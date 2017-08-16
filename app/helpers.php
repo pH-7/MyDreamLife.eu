bfee22a368ec1efe38f7c12b636415abba3f0fff
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-16 22:03:28 +0100

    Remove old unused helper funcs

--- app/helpers.php
+++ app/helpers.php
@@ -23,22 +23,3 @@ function redirect($url) {
 
     header('Location: .' . $url);
 }
-
-function error($type) {
-    echo $type;
-}
-
-function clean($key) {
-    return addslashes(htmlentities(trim($key)));
-}
-
-function s_excerpt($content, $end, $append) {
-    if (strlen($content) > $end) {
-        $excerpt = substr($content, 0, strrpos($content, ' '));
-        $excerpt .= $append;
-    } else {
-        $excerpt = $content;
-    }
-
-    return $excerpt;
-}
