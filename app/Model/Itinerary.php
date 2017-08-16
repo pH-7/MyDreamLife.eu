5f3664f28831126b394622cbe47b8a6d5a61970a
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-16 22:07:04 +0100

    Add Model (Itinerary) class

---
+++ app/Model/Itinerary.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+namespace Model;
+
+use Core\Database;
+
+class Itinerary
+{
+    public static function insert(array $binds)
+    {
+        Database::query("INSERT INTO itinerary (email, residence, nationality, destination, gender, age, lifestyle, background, job, saving, availability) VALUES(:email, :residence, :nationality, :destination, :gender, :age, :lifestyle, :background, :job, :saving, :availability)", $binds);
+    }
+}
