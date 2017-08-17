e45a5efbd3210bb9ce325203eea4c67c360b3797
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-17 23:06:58 +0100

    Add option for debuging the web app

--- app/app.php
+++ app/app.php
@@ -8,12 +8,19 @@
 use Core\Main;
 use Core\Database;
 
-ini_set('display_errors', 'on');
-
 define('APP_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 
 require 'config/site.php';
 require 'helpers.php';
+
+if (DEBUG_MODE) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+} else {
+    error_reporting(0);
+    ini_set('display_errors', 'Off');
+}
+
 
 function autoloadClasses($className) {
     $className = str_replace('\\', '/', $className);
