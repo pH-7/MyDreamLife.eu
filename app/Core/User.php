d2562a5a8eb04889ced7d5310ef3418dcf21daaa
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-17 01:00:40 +0100

    Remove unused/useless funcs for DreamLife purpose

--- app/Core/User.php
+++ app/Core/User.php
@@ -4,21 +4,8 @@ namespace Core;
 
 class User
 {
-    const KIK_AVATAR_URL = 'https://www.kik.me/u/';
-
     /** @var stdClass The user fields */
-    private static $_userData;
-
-    public static function store()
-    {
-        if (Main::loggedIn()) {
-            Database::query('SELECT * FROM `users` WHERE `user_id` = :userid', array(
-                ':userid' => Session::showCookie('userId')
-            ));
-
-            static::$_userData = Database::fetch();
-        }
-    }
+    private static $userData;
 
     /**
      * @return string|void
@@ -54,29 +41,7 @@ class User
     public static function userId()
     {
         if (static::loggedIn()) {
-            return static::$_userData->user_id;
-        }
-    }
-
-    /**
-     * @param string $kikUsername
-     *
-     * @return string
-     */
-    public static function getAvatar($kikUsername)
-    {
-        $url = self::KIK_AVATAR_URL . $kikUsername;
-        $opts = array('http' => array('header' => "User-Agent:MyAgent/1.0\r\n"));
-        $context = stream_context_create($opts);
-        $html = file_get_contents($url, false, $context);
-        $doc = new DOMDocument();
-        @$doc->loadHTML($html);
-        $xpath = new DOMXPath($doc);
-        $metaContentAttributeNodes = $xpath->query('/html/head/meta[@property="og:image"]/@content');
-
-        foreach ($metaContentAttributeNodes as $metaContentAttributeNode) {
-            $thumb = $metaContentAttributeNode->nodeValue;
-            return str_replace(array('thumb', 'http://'), array('orig', 'https://'), $thumb);
+            return static::$userData->user_id;
         }
     }
 }
