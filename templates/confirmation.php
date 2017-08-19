e6ab5ae5febfe2c588100865b7f42bbf2d2082c9
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-19 18:16:58 +0100

    Fix typo

--- templates/confirmation.php
+++ templates/confirmation.php
@@ -2,7 +2,7 @@
     <h2>Well Done! That's all for you!</h2>
 
     <p>We will review your application and we'll send you an email within 3-5 working days containing your full itinerary plan matching best with your criteria.<br />
-        You will be able to strat your dream life straight away by following the email and the TODO list that you will receive.
+        You will be able to start your dream life straight away by following the email and the TODO list that you will receive.
     </p>
 
     <p>Good Luck! (<em>and well done for the big step you just did and that most people will never do...</em>)</p>
