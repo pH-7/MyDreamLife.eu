a4b3740c967d19737ae48b86f6e24d84e44f4dd8
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-20 19:32:41 +0100

    Change div to article tag

--- templates/page/about.php
+++ templates/page/about.php
@@ -1,8 +1,8 @@
-<div class="center">
+<article class="center">
     <h3>Yes, OK, But ... <span class="underline">Why MyNewDream.eu?</span></h3>
 
     <p>My name is ...</p>
     <p>I wanted to travel in ...</p>
     <p>but ...</p>
 
-</div>
+</article>
