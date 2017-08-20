d6769b144a6e50f191ce2686f10c31bc93f838bc
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-20 22:43:06 +0100

    Minor post page improvements
    Add Itinerary button
    
    Add new CSS class for adding margin-bottom

--- templates/page/post.php
+++ templates/page/post.php
@@ -1,8 +1,12 @@
-<section class="center">
+<section class="center bottom-margin">
     <h3><?php echo $title ?></h3>
     <article>
         <iframe src="https://player.vimeo.com/video/<?php echo $vimeoId ?>" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         <p><?php echo $description ?></p>
         <p>See also the <a href="<?php echo site_url('apps') ?>" rel="nofollow noopener" target="_blank" class="underline">useful apps</a> to get before your trip.'</p>
+
+        <div class="">
+            <a href="<?php echo site_url() ?>" class="waves-effect orange-light btn-large">Get Your Itinerary</a>
+        </div>
     </article>
 </section>
