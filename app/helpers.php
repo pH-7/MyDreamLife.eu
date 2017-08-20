251ceae82ee51b4856ecd20d76d82c6717cfedc5
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-20 19:05:17 +0100

    Add escape func in helper

--- app/helpers.php
+++ app/helpers.php
@@ -16,11 +16,15 @@ function asset_url(string $var = '') {
     return SITE_URL . 'assets/';
 }
 
-function redirect($url) {
+function redirect(string $url) {
     if (strpos($url, 'http') === false) {
         $url = SITE_URL . $url;
     }
 
     header('Location: ' . $url);
     exit;
+}
+
+function escape(string $value) {
+    return htmlspecialchars($value, ENT_QUOTES);
 }
