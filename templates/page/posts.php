064f88ad21e89d175853e6fe6bcb147844eb4879
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-20 22:18:36 +0100

    Bugfix: Need to use lt operator

--- templates/page/posts.php
+++ templates/page/posts.php
@@ -5,7 +5,7 @@
             <p><?php echo mb_strimwidth(strip_tags($posts[$i]['description']), 0, 150, '...') ?></p>
             <p><a href="<?php echo site_url('post?id=' . $i) ?>">See The Post &rarr;</a></p>
 
-            <?php if ($i ==! $totalPosts): ?>
+            <?php if ($i < $totalPosts): ?>
                 <hr />
             <?php endif ?>
         </article>
