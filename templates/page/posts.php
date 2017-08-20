dd8778cc129301cfd8a2b6d8281b72d70f58f1fa
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-20 19:31:20 +0100

    Ending a tag missing + Add "Continue Reading" link

--- templates/page/posts.php
+++ templates/page/posts.php
@@ -1,8 +1,9 @@
 <section class="center">
     <?php for ($i = 1, $totalPosts = count($posts); $i <= $totalPosts; $i++): ?>
         <article>
-            <h3><a href="<?php echo site_url('post?id=' . $i) ?>"><?php echo $posts[$i]['title'] ?></h3>
+            <h3><a href="<?php echo site_url('post?id=' . $i) ?>"><?php echo $posts[$i]['title'] ?></a></h3>
             <p><?php echo mb_strimwidth(strip_tags($posts[$i]['description']), 0, 150, '...') ?></p>
+            <p><a href="<?php echo site_url('post?id=' . $i) ?>">See The Post &rarr;</a></p>
 
             <?php if ($i ==! $totalPosts): ?>
                 <hr />
