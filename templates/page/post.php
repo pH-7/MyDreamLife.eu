118b5cffc69b7cbaf0e2ad3403a986229cf25388
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-20 19:24:33 +0100

    Change div to section to regroup post contents

--- templates/page/post.php
+++ templates/page/post.php
@@ -1,8 +1,8 @@
-<div class="center">
+<section class="center">
     <h3><?php echo $title ?></h3>
     <article>
         <iframe src="https://player.vimeo.com/video/<?php echo $vimeoId ?>" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         <p><?php echo $description ?></p>
         <p>See also the <a href="<?php echo site_url('apps') ?>" rel="nofollow noopener" target="_blank" class="underline">useful apps</a> to get before your trip.'</p>
     </article>
-</div>
+</section>
