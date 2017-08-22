e642b42cc9fcc99135e00bf2aa53d51d80050682
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-22 22:50:52 +0100

    Add unit tests

---
+++ tests/Core/Translator/CountryTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types = 1);
+
+namespace Test\Core\Translator;
+
+use Core\Translator\Country;
+use PHPUnit\Framework\TestCase;
+
+class CountryTest extends TestCase
+{
+    public function testValidValue(): void
+    {
+        $country = new Country('be');
+        $this->assertSame('Belgium', $country->getValue());
+    }
+
+    /**
+     * @param mixed $countryCode
+     *
+     * @dataProvider invalidCountryCodesProvider
+     * @expectedException \Core\Translator\Exception\InvalidFormatException
+     */
+    public function testInvalidValue($countryCode): void
+    {
+        new Country($countryCode);
+    }
+
+    public function invalidCountryCodesProvider(): array
+    {
+        return [
+            [''],
+            ['a'],
+            ['ii'],
+            ['djdjdj']
+        ];
+    }
+}
---
+++ tests/Core/Translator/NationalityTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types = 1);
+
+namespace Test\Core\Translator;
+
+use Core\Translator\Nationality;
+use PHPUnit\Framework\TestCase;
+
+class NationalityTest extends TestCase
+{
+    public function testValidValue(): void
+    {
+        $country = new Nationality('be');
+        $this->assertSame('Belgian', $country->getValue());
+    }
+
+    /**
+     * @param mixed $nationalityCode
+     *
+     * @dataProvider invalidNationalityCodesProvider
+     * @expectedException \Core\Translator\Exception\InvalidFormatException
+     */
+    public function testInvalidValue($nationalityCode): void
+    {
+        new Nationality($nationalityCode);
+    }
+
+    public function invalidNationalityCodesProvider(): array
+    {
+        return [
+            [''],
+            ['a'],
+            ['ii'],
+            ['djdjdj']
+        ];
+    }
+}
