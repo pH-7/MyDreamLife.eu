e5afa37b50166008703f821a5f79cf862094938e
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-23 14:49:53 +0100

    Add Lifestyle class to translate lifestyle types

---
+++ app/Core/Translator/Lifestyle.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types = 1);
+
+namespace Core\Translator;
+
+use Core\Translator\Exception\InvalidFormatException;
+
+class Lifestyle
+{
+    /** @var string */
+    private $lifestyle;
+
+    /** @var array */
+    private static $lifestyleNames = [
+        'big-cities' => 'Big Cities',
+        'adventure' =>'Adventure',
+        'nature' => 'Nature',
+        'nightlife' => 'Nightlife, Bars, ...',
+    ];
+
+    /**
+     * @param string $lifestyle
+     *
+     * @throws InvalidFormatException
+     */
+    public function __construct(string $lifestyle)
+    {
+        if (
+            empty(self::$lifestyleNames[$lifestyle])
+        ) {
+            throw new InvalidFormatException(sprintf('%s is an invalid lifestyle type.', $lifestyle));
+        }
+
+        $this->lifestyle = $lifestyle;
+    }
+
+    /**
+     * Returns the lifestyle name.
+     *
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return self::$lifestyleNames[$this->lifestyle];
+    }
+}
---
+++ tests/Core/Translator/LifestyleTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types = 1);
+
+namespace Test\Core\Translator;
+
+use Core\Translator\Lifestyle;
+use PHPUnit\Framework\TestCase;
+
+class LifestyleTest extends TestCase
+{
+    public function testValidValue(): void
+    {
+        $lifestyle = new Lifestyle('adventure');
+        $this->assertSame('Adventure', $lifestyle->getValue());
+    }
+
+    /**
+     * @expectedException \Core\Translator\Exception\InvalidFormatException
+     */
+    public function testInvalidValue(): void
+    {
+        new Lifestyle('blablabla');
+    }
+}
