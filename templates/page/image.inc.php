49859ee958ba8beeb1d9dfde46eea4f26e9f6a47
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-23 14:18:27 +0100

    Set image height to 300px

--- templates/page/image.inc.php
+++ templates/page/image.inc.php
@@ -1,1 +1,1 @@
-<img src="<?php echo $imageUrl ?>" width="600" height="400" alt="<?php echo $title ?>" title="<?php echo $title ?>">
+<img src="<?php echo $imageUrl ?>" width="600" height="300" alt="<?php echo $title ?>" title="<?php echo $title ?>">
