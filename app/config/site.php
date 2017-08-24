39da456fecee3049260f44561aa329a5a99eb3b6
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-24 21:44:36 +0100

    Add informative comment for SITE_URL value

--- app/config/site.php
+++ app/config/site.php
@@ -1,7 +1,7 @@
 <?php
 
 // Edit the following details by yours
-define('SITE_URL', 'http://localhost:8888/mydreamlife/');
+define('SITE_URL', 'http://localhost:8888/mydreamlife/'); // Must end by a slash
 define('SITE_NAME', 'MyDreamLife.eu');
 define('ADMIN_EMAIL', 'hi@ph7.me');
 define('DEBUG_MODE', true);
