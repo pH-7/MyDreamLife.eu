ea9131402e13c99ed5be9e30defea1124a58ae5f
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-27 22:40:37 +0100

    Better placeholder text

--- templates/get-results.php
+++ templates/get-results.php
@@ -2,7 +2,7 @@
     <h2>My Results</h2>
 
     <form method="post" action="<?php echo site_url('confirmation') ?>">
-        <input type="email" name="email" placeholder="Email Address" required>
+        <input type="email" name="email" placeholder="Main Email Address" required>
         <input type="text" name="name" class="nofield"> <!-- Spambot prevention using a hidden field -->
         <button type="submit" class="waves-effect waves-light btn-large">
             <i class="material-icons right">send</i> Get My LIFE itinerary
