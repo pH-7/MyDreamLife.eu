4a4a2384caaf78c29b336222f5e98b812af420dd
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-27 17:50:15 +0100

    Add Unsplash Class to generate image URL easily

---
+++ tests/Core/Translator/ItineraryTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types = 1);
+
+namespace Test\Core\Translator;
+
+use PHPUnit\Framework\TestCase;
+
+class ItineraryTest extends TestCase
+{
+
+}
