6241037457fb8d2aaf6cd7a1bec12052ad0e090c
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-09 00:58:03 -0800

    Add Input/Session unit tests

---
+++ tests/Core/InputTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types = 1);
+
+namespace Test\Core\Input;
+
+use Core\Input;
+use PHPUnit\Framework\TestCase;
+
+class InputTest extends TestCase
+{
+    public function testPost(): void
+    {
+        $_POST['hi'] = 'Hi Am!';
+
+        $this->assertSame('Hi Am!', Input::post('hi'));
+    }
+
+    public function testUnsetPostKey(): void
+    {
+        $this->assertFalse(Input::post('nothing'));
+    }
+
+    public function testGet(): void
+    {
+        $_GET['hi'] = 'I Am!';
+
+        $this->assertSame('I Am!', Input::get('hi'));
+    }
+
+    public function testUnsetGetKey(): void
+    {
+        $this->assertFalse(Input::get('lalala'));
+    }
+
+    protected function tearDown()
+    {
+        unset($_POST, $_GET);
+    }
+}
