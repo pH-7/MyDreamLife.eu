7bc08ad6c19cd099a6c145614a820b7ed45c7ba1
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-11 00:43:15 -0800

    Save project to GitLab too!

--- save-code.sh
+++ save-code.sh
@@ -3,6 +3,11 @@ git remote rm origin
 git remote add origin git@bitbucket.org:pH_7/mydreamlife.git
 git push
 
+# GitLab repo
+git remote rm origin
+git remote add origin git@gitlab.com:pH-7/MyDreamLife.git
+git push
+
 # GitHub repo
 git remote rm origin
 git remote add origin git@github.com:pH-7/MyDreamLife.eu.git
