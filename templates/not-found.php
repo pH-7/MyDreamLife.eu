b351867ab328a6a0057093e17b4c23557f7b1a3e
Pierre-Henry Soria <hi@ph7.me>
2019-02-24 14:39:26 +1300

    [CleanUp] Better readable HTML indentation :)

--- templates/not-found.php
+++ templates/not-found.php
@@ -4,11 +4,16 @@
             <div class="col-md-4 col-md-offset-4">
                 <div class="panel">
                     <h2>Page Not Found!</h2>
+
                     <p><strong>Sorry, we weren't able to find the page you requested.</strong></p>
                     <p><strong><em>Suggestions:</em></strong></p>
+
                     <ul>
                         <li><a href="<?php echo site_url(); ?>">Main Page</a></li>
-                        <li><a href="javascript:void(0);" onclick="history.go(-1);">Go back to the previous page</a>
+                        <li>
+                            <a href="javascript:void(0);" onclick="history.go(-1);">
+                                Go back to the previous page
+                            </a>
                         </li>
                     </ul>
                 </div>
--- templates/page/posts.php
+++ templates/page/posts.php
@@ -1,9 +1,19 @@
 <section class="center">
     <?php for ($postIndex = 1, $totalPosts = count($posts); $postIndex <= $totalPosts; $postIndex++): ?>
         <article>
-            <h3><a href="<?php echo site_url("p/{$posts[$postIndex]['uri']}/$postIndex") ?>"><?php echo $posts[$postIndex]['title'] ?></a></h3>
-            <p><?php echo mb_strimwidth(strip_tags($posts[$postIndex]['description']), 0, 150, '...') ?></p>
-            <p><a href="<?php echo site_url("p/{$posts[$postIndex]['uri']}/$postIndex") ?>">See The Post &rarr;</a></p>
+            <h3>
+                <a href="<?php echo site_url("p/{$posts[$postIndex]['uri']}/$postIndex") ?>">
+                    <?php echo $posts[$postIndex]['title'] ?></a>
+            </h3>
+
+            <p>
+                <?php echo mb_strimwidth(strip_tags($posts[$postIndex]['description']), 0, 150, '...') ?>
+            </p>
+            <p>
+                <a href="<?php echo site_url("p/{$posts[$postIndex]['uri']}/$postIndex") ?>">
+                    See The Post &rarr;
+                </a>
+            </p>
 
             <?php if ($postIndex < $totalPosts): ?>
                 <hr>
