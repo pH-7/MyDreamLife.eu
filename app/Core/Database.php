75e0bb19df8abe7d3faadbfd30db2485ae94cbaa
Pierre-Henry Soria <hi@ph7.me>
2022-04-07 17:53:54 +0930

    Fixed return type declaration

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -6,7 +6,6 @@ namespace Core;
 
 use PDO;
 use PDOException;
-use stdClass;
 
 class Database
 {
@@ -64,11 +63,11 @@ class Database
     /**
      * Returns a single row.
      *
-     * @return stdClass|null The row as an object.
+     * @return mixed The row as an object.
      */
-    public static function fetch(): ?stdClass
+    public static function fetch()
     {
-        return static::$stmt->fetch(PDO::FETCH_OBJ) ?? null;
+        return static::$stmt->fetch(PDO::FETCH_OBJ);
     }
 
     public static function quote(string $string): string
